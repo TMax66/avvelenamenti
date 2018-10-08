49b5700d19fd605d81aa93498d79480f983f1ceb
TMax66 <massimo.tranquillo@gmail.com>
2018-10-08 16:03:49 +0200

    update codici

--- codici.r
+++ codici.r
@@ -1,6 +1,14 @@
 library(googlesheets)
+library(tidyverse)
+library(leaflet)
+library(maps)
 
 
-dati<-gs_title("AMR")
-ds <-gs_read(dati, ws="AMR" )
 
+dati<-gs_title("avvelenamenti")
+ds <-gs_read(dati, ws="avv" )
+
+###############################
+
+leaflet(options = leafletOptions(minZoom = 0, maxZoom = 18))
+IT<-map('italy', fill = TRUE, col = 1:10)
