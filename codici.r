b219f25b24009f4e78472be5e857fc04acc07464
TMax66 <massimo.tranquillo@gmail.com>
2018-10-08 23:02:47 +0200

    update codici

--- codici.r
+++ codici.r
@@ -2,7 +2,8 @@ library(googlesheets)
 library(tidyverse)
 library(leaflet)
 library(maps)
-
+library(rgdal)
+library(sp)
 
 
 dati<-gs_title("avvelenamenti")
@@ -11,4 +12,22 @@ ds <-gs_read(dati, ws="avv" )
 ###############################
 
 leaflet(options = leafletOptions(minZoom = 0, maxZoom = 18))
-IT<-map('italy', fill = TRUE, col = 1:10)
+IT<-map('italy', fill = TRUE, plot=FALSE)
+
+
+leaflet(data = IT) %>% addTiles() %>%
+  addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
+
+m <- leaflet() %>% setView(lng = 9.6320701, lat =45.693177, zoom = 9)
+m %>% addTiles()
+
+bg<-readOGR(dsn="shp", layer = "Comuni_2012_polygon")
+bg<-spTransform(bg, CRS("+proj=longlat +datum=WGS84"))
+centroidi<-coordinates(bg)
+points(centroidi, col="black", pch=20)
+
+
+####################
+
+london <- readOGR(dsn = "london", layer = "london_sport")
+london<-spTransform(london, CRS("+proj=longlat +datum=WGS84"))
