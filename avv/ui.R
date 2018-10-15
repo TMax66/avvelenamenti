42bcb3232ea2880b260cfc4f5455e2c24713adf6
TMax66 <massimo.tranquillo@gmail.com>
2018-10-15 22:36:12 +0200

    update

--- avv/ui.R
+++ avv/ui.R
@@ -2,27 +2,28 @@ header<-dashboardHeader(title="OM 28/06/2018 Esche/Bocconi e Avvelenamenti")
 
 body<-dashboardBody(
   fluidRow(
-    column(width = 7,
-           box(width = NULL, solidHeader = TRUE,
-               leafletOutput("map", height=600)),
-               
-        
-           box(width=NULL,solidHeader = TRUE,
-           dataTableOutput("tabella")
-           )
-    ),
     column(width=5,
-            box(width=NULL, 
-              sliderInput("anno", "Anno", min=2002, max=2018, value=2018),
+           box(width=NULL, 
+               sliderInput("anno", "Anno", min=2002, max=2018, value=2018),
                
-              br(),
+               br(),
                selectInput("specie", "Specie/Esca Boccone",
                            c(unique(as.character(avv$specie))), selected = "CANE"),
                hr(),
                br(),
                plotOutput("p")
+           )
+    ),
+    column(width = 7,
+           box(width = NULL, solidHeader = TRUE,
+               leafletOutput("map", height=400)),
+               
+        
+           box(width=NULL,solidHeader = TRUE,
+           dataTableOutput("tabella")
+           )
     )
-  )
+
 ))
 
 dashboardPage(
